abf8df393a57113a03bf200448b1bfe28976253d
Mohamed Ramadan <123234014+Mohamed-Ramadan1@users.noreply.github.com>
2024-04-20 12:24:07 +0200

    reviews slice is completed

--- src/layout/mainLayout/MainLayout.jsx
+++ src/layout/mainLayout/MainLayout.jsx
@@ -1,30 +1,5 @@
-import { login } from "../../store/userSlice";
-import { useDispatch, useSelector } from "react-redux";
-
 const MainLayout = () => {
-  const dispatch = useDispatch();
-  const { user, isLoggedIn } = useSelector((state) => state.userReducers);
-
-  const userData = { email: "ahmedMakram@gmail.com", password: "1234566789" };
-
-  const loginHandler = () => {
-    dispatch(login(userData));
-  };
-
-  return (
-    <div>
-      {!user && (
-        <button onClick={loginHandler} className="block">
-          login
-        </button>
-      )}
-      {isLoggedIn && (
-        <>
-          <h1>Welcome {user.name}</h1>
-        </>
-      )}
-    </div>
-  );
+  return <div>done</div>;
 };
 
 export default MainLayout;
