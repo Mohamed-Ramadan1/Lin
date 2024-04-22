1716ffbc117e422661e760f8df2ab9048dd81a6e
Mohamed Ramadan <123234014+Mohamed-Ramadan1@users.noreply.github.com>
2024-04-22 09:25:46 +0200

    create the login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,65 @@
+import CustomInput from "../components/forms/CustomInput";
+import { Form, Formik } from "formik";
+import { useDispatch, useSelector } from "react-redux";
+import { userActions } from "./../store/userSlice";
+import { toast } from "react-toastify";
+import { loginSchema } from "./../schema/authFormsSchema";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { login } from "./../store/userSlice";
+
 const Login = () => {
-  return <div>Log-in</div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isSuccess } = useSelector((state) => state.userReducers);
+
+  useEffect(() => {
+    if (isSuccess) {
+      toast.success("User created successfully");
+      dispatch(userActions.resetSuccessStates());
+      return navigate("/");
+    }
+  }, [isSuccess, dispatch, navigate]);
+
+  return (
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+      }}
+      validationSchema={loginSchema}
+      onSubmit={(values, actions) => {
+        dispatch(login(values));
+        actions.resetForm();
+      }}
+    >
+      {({ handleSubmit }) => (
+        <Form onSubmit={handleSubmit} className="">
+          <CustomInput
+            type="email"
+            name="email"
+            label="email"
+            id="email"
+            placeholder="email"
+          />
+          <CustomInput
+            type="password"
+            name="password"
+            label="password"
+            id="password"
+            placeholder="password"
+          />
+
+          <button
+            type="submit"
+            className="bg-gray-200 w-full p-2 rounded-sm mb-2"
+          >
+            Sign Up
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
 };
 
 export default Login;
