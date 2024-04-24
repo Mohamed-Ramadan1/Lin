8a1b2972dcc134202c1b920ec55db32b9aa7ff44
Ahmed Magdy <83177544+3AMEER@users.noreply.github.com>
2024-04-24 19:47:50 +0200

    Update Content

--- src/components/common/swipper/CardCoursPay.jsx
+++ src/components/common/swipper/CardCoursPay.jsx
@@ -7,7 +7,7 @@ export const CardCoursPay = () => {
     return (
         <div className='w-fill bg-[#2B2B2B] p-[10px] rounded-[20px]'>
 
-        <div className="w-[300px] flex flex-col items-start gap-[15px]">
+        <div className="w-[300px] flex flex-col items-start gap-[15px] max-sm:w-[270px]">
   
           <div className="image w-full relative rounded-[10px] overflow-hidden">
   
