666473b94124fee0dbe6f9efe3bd9b43b3c60cbb
Mohamed Ramadan <123234014+Mohamed-Ramadan1@users.noreply.github.com>
2024-04-27 00:08:50 +0300

    adding some development images and updated the existed content with seprated some components from the main layout .

--- src/components/common/ViewCoursesComponents/CardCourse.jsx
+++ src/components/common/ViewCoursesComponents/CardCourse.jsx
@@ -1,63 +1,73 @@
-import React from "react";
+import RatingStars from "./RatingStars";
 
-import imgCourse from '../../../assets/imgCourse.png'
+import webdev from "../../../assets/courseImages/webdev.jpg";
 
-const CardCourse = () => {
+const CardCourse = ({ course }) => {
+  let {
+    title,
+    description,
+    duration,
+    price,
+    totalReviews,
+    averageRating,
+    instructor,
+    photo,
+    language,
+  } = course;
+
+  averageRating = parseInt(averageRating);
   return (
-    <div href="" className="cardCourse p-[10px] flex items-center justify-center gap-[10px] hover:bg-[#f5f5f5] max-md:p-[0px] max-md:justify-start max-md:items-start ">
-
+    <div
+      href=""
+      className="cardCourse p-[10px] flex items-center justify-center gap-[10px] hover:bg-[#f5f5f5] max-md:p-[0px] max-md:justify-start max-md:items-start "
+    >
       {/* Image Course  */}
       <div className="imgCourse border-[1px] border-[#e2e2e2] max-w-[250px] rounded-[10px] max-md:max-w-[100px] max-md:min-w-[50px] max-md:rounded-none ">
-        <img src={imgCourse} alt="" className="w-full h-full object-cover rounded-[10px] max-md:w-full max-md:rounded-none"/>
+        <img
+          src={webdev}
+          alt=""
+          className="w-full h-full object-cover rounded-[10px] max-md:w-full max-md:rounded-none"
+        />
       </div>
 
-      
       {/* Details Course */}
       <div className="content flex flex-col items-start gap-[5px]">
-
-        <span className="py-[2px] px-[10px] text-[10px] font-medium text-[#9747FF] bg-[#efe2ff] rounded-[100px]">DR : Ahmed</span>
+        <span className="py-[2px] px-[10px] text-[10px] font-medium text-[#9747FF] bg-[#efe2ff] rounded-[100px]">
+          DR : Ahmed
+        </span>
 
         <div className="info w-full flex flex-col items-start gap-[5px]">
-          <h1 className="text-2xl font-bold text-[#333] max-md:text-base">UI / UX Design - (Arabic)</h1>
-          <p className="text-base font-medium text-[#6e6e6e] max-md:font-light max-md:truncate max-md:max-w-[200px]">Become a Full-Stack Web Developer with just ONE course. HTML, CSS, Javascript, Node, React, PostgreSQL, Web3 and DApps</p>
+          <h2 className="text-2xl font-bold text-[#333] max-md:text-base">
+            {title} - {language}
+          </h2>
+          <p className="text-base font-medium text-[#6e6e6e] max-md:font-light max-md:truncate max-md:max-w-[200px]">
+            {description}
+          </p>
         </div>
 
         <div className="rating flex gap-[15px] justify-center items-center max-md:gap-2 ">
+          <h3 className="text-[#444444] text-[17px] font-bold max-md:text-[13px]">
+            {averageRating}
+          </h3>
 
-        <h3 className='text-[#444444] text-[17px] font-bold max-md:text-[13px]'>4.3</h3>            
-                
-        <div className="stars flex items-center gap-[4px]">
-                    
-            <div className="icon">
-                <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 0L9.79611 5.52786H15.6085L10.9062 8.94427L12.7023 14.4721L8 11.0557L3.29772 14.4721L5.09383 8.94427L0.391548 5.52786H6.20389L8 0Z" fill="#FF7A00"/></svg>            
-            </div>
-            <div className="icon">
-                <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 0L9.79611 5.52786H15.6085L10.9062 8.94427L12.7023 14.4721L8 11.0557L3.29772 14.4721L5.09383 8.94427L0.391548 5.52786H6.20389L8 0Z" fill="#FF7A00"/></svg>            
-            </div>
-            <div className="icon">
-                <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 0L9.79611 5.52786H15.6085L10.9062 8.94427L12.7023 14.4721L8 11.0557L3.29772 14.4721L5.09383 8.94427L0.391548 5.52786H6.20389L8 0Z" fill="#FF7A00"/></svg>            
-            </div>
-            <div className="icon">
-                <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 0L9.79611 5.52786H15.6085L10.9062 8.94427L12.7023 14.4721L8 11.0557L3.29772 14.4721L5.09383 8.94427L0.391548 5.52786H6.20389L8 0Z" fill="#D9D9D9" /></svg>
-            </div>
-            <div className="icon">
-                <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 0L9.79611 5.52786H15.6085L10.9062 8.94427L12.7023 14.4721L8 11.0557L3.29772 14.4721L5.09383 8.94427L0.391548 5.52786H6.20389L8 0Z" fill="#D9D9D9" /></svg>
-            </div>
+          <div className="stars flex items-center gap-[4px]">
+            <RatingStars rating={averageRating} />
+          </div>
+
+          <div>
+            <span className="text-[#5e5e5e] max-md:text-[10px]">
+              ( {totalReviews} ) reviews
+            </span>
+          </div>
         </div>
-                
-        <div>
-            <span className='text-[#5e5e5e] max-md:text-[10px]'>( 90,342 )</span>            
-        </div>
-                
-        </div>
-            
+
         <div className="price flex gap-[10px] items-end">
-        <h1 className='text-[25px] font-bold text-[#222]  max-sm:text-[20px]'>149,99</h1>
-        <span className='text-base text-[#636363] line-through'>155,99</span>
+          <h1 className="text-[25px] font-bold text-[#222]  max-sm:text-[20px]">
+            {price.toFixed(2)}
+          </h1>
+          <span className="text-base text-[#636363] line-through">155,99</span>
         </div>
-        
       </div>
-
     </div>
   );
 };
--- src/pages/ViewCourses.jsx
+++ src/pages/ViewCourses.jsx
@@ -1,59 +1,36 @@
 // Framer Motion
 import { motion } from "framer-motion";
-
+import CoursesPageIntro from "../components/common/ViewCoursesComponents/CoursesPageIntro";
 // Components
 import CardCourse from "../components/common/ViewCoursesComponents/CardCourse";
 import FilterCourses from "../components/common/ViewCoursesComponents/FilterCourses";
-
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getAllCourses } from "./../store/courseSlice";
 const ViewCourses = () => {
+  const dispatch = useDispatch();
+  const { courses, error, loading } = useSelector(
+    (state) => state.courseReducers
+  );
+
+  useEffect(() => {
+    dispatch(getAllCourses());
+  }, [dispatch]);
+
   return (
     <div className="viewCoursePage relative flex justify-center items-center px-[124px] py-[70px] max-2xl:px-[30px] max-sm:py-[30px] max-sm:px-[15px]">
-      <div className="container max-w-[1400px] m-auto flex flex-col gap-10 flex-col justify-center items-center">
-        <div className="title flex flex-col items-start gap-[15px]">
-          <div className="info">
-            <motion.h1
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ type: "tween", delay: 0.4 }}
-              className="text-4xl font-bold text-[#222] max-md:text-3xl"
-            >
-              All Business courses
-            </motion.h1>
-            <motion.p
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ type: "tween", delay: 0.5 }}
-              className="text-[20px] mt-[10px] text-[#8d8d8d] font-bold"
-            >
-              Courses to get you started
-            </motion.p>
-          </div>
-          <p className="text-base text-[#8d8d8d]">
-            All Business courses" encompass a wide array of educational
-            offerings designed to equip individuals with the knowledge and
-            skills necessary to thrive in various aspects of the business world.
-            These courses cover topics ranging from entrepreneurship and
-            management to finance, marketing, and beyond. Here's an overview of
-            what you can expect from this category
-          </p>
-        </div>
-
+      <div className="container max-w-[1400px] m-auto flex flex-col gap-10  justify-center items-center">
+        <CoursesPageIntro />
         <div className="sub-container flex gap-[10px] flex-row w-full">
           <div className="basis-1/3 h-fit rounded-[10px] p-[10px] bg-[#F6F6F6] max-lg:hidden">
             <FilterCourses />
           </div>
 
           <div className="basis-full flex flex-col gap-[20px] max-md:gap-[30px]">
-            <CardCourse />
-            <CardCourse />
-            <CardCourse />
-            <CardCourse />
-            <CardCourse />
-            <CardCourse />
-            <CardCourse />
-            <CardCourse />
-            <CardCourse />
-            <CardCourse />
+            {courses &&
+              courses.map((course) => (
+                <CardCourse key={course._id} course={course} />
+              ))}
           </div>
         </div>
       </div>
