79966756fa4c7eb45e60c373c29deb7b036a1255
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2024-05-07 15:40:57 +0300

    update nave with logout functionlity

--- src/layout/header/TopContent.jsx
+++ src/layout/header/TopContent.jsx
@@ -1,12 +1,34 @@
 import TopContentNavigationLinks from "./TopContentNavigationLinks";
-
+import { RiLogoutBoxRLine } from "react-icons/ri";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../../store/userSlice";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 const TopContent = () => {
+  const { user } = useSelector((state) => state.userReducers);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   return (
     <div className="topContent relative p-[15px] bg-[#1D2026]">
       <div className="container max-w-[1700px] m-auto gap-10 flex justify-between items-start">
-        <div className="links">
+        <div className="links  ">
           <TopContentNavigationLinks />
         </div>
+        {user && (
+          <button
+            type="button"
+            onClick={() => {
+              dispatch(logout());
+              toast.success("Logged out successfully");
+              navigate("/");
+            }}
+            className="btn flex items-center gap-2 px-4 py-2 text-[#fff] bg-[#9747FF]"
+          >
+            <RiLogoutBoxRLine />
+            Logout
+          </button>
+        )}
       </div>
     </div>
   );
